0d54ed6a07c16529c20a3617cedde80e0810974d
Nikita Davids <Nikita Davids@KITA>
2022-09-07 13:06:53 +0200

    Add project files.

---
+++ Spar3030 ICE TASK/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Spar3030_ICE_TASK
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
